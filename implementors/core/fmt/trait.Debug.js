0b6a32579cc55b618d4e89f017f9b17b6fdd9d26
Travis CI User <travis@example.org>
2017-09-17 14:24:44 +0000

    Documentation for rust-lang-nursery/log@8af5f3c

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["env_logger"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"env_logger/enum.Target.html\" title=\"enum env_logger::Target\">Target</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
